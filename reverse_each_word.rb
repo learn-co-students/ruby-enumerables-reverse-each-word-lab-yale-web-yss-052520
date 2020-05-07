67a14cdba2c0b1bd0f070f487519a515bff5328f
Eric  Ou <eric.ou@yale.edu>
2020-05-07 04:50:06 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,3 @@
 def reverse_each_word(str)
-  arr = str.split(" ")
-  r_arr = arr.collect {|word| word.reverse}
-  r_arr.join(" ")
+  arr = str.split(" ").collect {|word| word.reverse}.join(" ")
 end 
